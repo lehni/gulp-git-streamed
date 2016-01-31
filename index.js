7d76ba66ef54baff6a08b9862ec6aaca2adb6917
Jürg Lehni <juerg@scratchdisk.com>
2016-01-31 03:07:39 +0100

    Simplify parameter handling.
    We can use Function#length instead of the regular expression hack.

--- index.js
+++ index.js
@@ -8,24 +8,22 @@ var methods = [
 ];
 
 methods.forEach(function(method) {
-    // Get the original gulp-git function and determine its expected parameter
-    // count, to figure out where to pass the callback function (always last).
-    var func = git[method],
-        paramCount =  func.toString().match(/^\s*function[^\(]*\(([^\)]*)/)[1]
-            .split(',').length;
+    // Get the original gulp-git function. `func.length` will be its expected
+    // parameter count, and we'll use it to pass the callback function last.
+    var func = git[method];
     // Override the method with one that returns a stream and handles the
     // callback and errors for us.
     git[method] = function() {
         // Convert arguments to an array, and make sure we have the expected
         // amount of parameters.
-        var args = Array.prototype.slice.call(arguments, 0, paramCount);
+        var args = Array.prototype.slice.call(arguments, 0, func.length);
         return through.obj(function(file, enc, cb) {
             // No transformation needed, just pass it through without errors.
             cb(null, file);
         }, function(cb) {
             // Define the callback function as the last expected argument,
             // and call the original gulp-git function with it.
-            args[paramCount - 1] = function(err) {
+            args[func.length - 1] = function(err) {
                 if (err)
                     this.emit('error', err);
                 cb();
